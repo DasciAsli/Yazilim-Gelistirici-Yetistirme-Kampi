1a2aff97a4b7a4090c3cf2cd545fb04ec7b2a59c
AsliDasci <aslihandasci1@gmail.com>
2022-08-19 14:13:28 +0300

    Ders-3

--- KampIntro/Matematik/DortIslemManager.cs
+++ KampIntro/Matematik/DortIslemManager.cs
@@ -13,21 +13,5 @@ namespace Matematik
             int toplam = sayi1 + sayi2;
             Console.WriteLine("Sonuc : " + toplam);
         }
-        public void Cikar(int sayi1, int sayi2)
-        {
-            int fark = sayi1 + sayi2;
-            Console.WriteLine("Sonuc : " + fark);
-            
-        }
-        public void Carp(int sayi1, int sayi2)
-        {
-            int carpim = sayi1 * sayi2;
-            Console.WriteLine("Sonuc : " + carpim);
-        }
-        public void Böl(int sayi1, int sayi2)
-        {
-            int bölüm = sayi1 / sayi2;
-            Console.WriteLine("Sonuc : " + bölüm);
-        }
     }
 }
